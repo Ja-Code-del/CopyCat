0d639b8edb89f2db2f1ce2c8ba02154b37b27bb7
Bret Walda <82909109+Ja-Code-del@users.noreply.github.com>
2024-09-01 14:47:49 +0000

    Initial Works on Crosser
    now the crosser object can appear to screen but he needs to move ...

--- crossy_road/crosser.py
+++ crossy_road/crosser.py
@@ -1,4 +1,16 @@
-class Crosser:
+from turtle import Turtle
+
+INITIAL_POSITION = (0, -320)
+
+
+class Crosser(Turtle):
     """The crosser class"""
+
     def __init__(self):
-        pass
+        super().__init__()
+        self.shape("square")
+        self.color("gray")
+        self.penup()
+        self.goto(INITIAL_POSITION)
+
+

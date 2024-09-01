6931593e46e9b836384cb2c38c891d3bd8701e8c
Bret Walda <82909109+Ja-Code-del@users.noreply.github.com>
2024-09-01 15:40:43 +0000

    work on scoreboard
    initial work on scoreboard : display the score

--- crossy_road/main.py
+++ crossy_road/main.py
@@ -1,15 +1,17 @@
 from turtle import Screen
 from crosser import Crosser
-
+from scoreboard import ScoreBoard
 #scree setup
 screen = Screen()
 screen.colormode(255)
-screen.screensize(640, 480)
+screen.setup(640, 480)
 screen.bgcolor(33, 33, 33)
 
 #display the crosser named pitt
 screen.tracer(0)
 pitt = Crosser()
+
+board = ScoreBoard()
 #scree listen to events
 screen.listen()
 
--- crossy_road/scoreboard.py
+++ crossy_road/scoreboard.py
@@ -1,7 +1,20 @@
 from turtle import Turtle
 
+ALIGNMENT = "left"
+FONT = ("Arial", 18, "normal")
+BIGFONT = ("Courrier", 96, "normal")
+
 
 class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
-        
+        self.hideturtle()
+        self.score = 0
+        self.penup()
+        self.color("white")
+        self.goto(-310, 210)
+        self.score_view()
+
+    def score_view(self):
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+

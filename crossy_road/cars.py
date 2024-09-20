8ebacaa926a1282342c282d839615650bfbc8422
Bret Walda <82909109+Ja-Code-del@users.noreply.github.com>
2024-09-20 01:34:28 +0000

    most stable version. now work on score and stages

--- crossy_road/cars.py
+++ crossy_road/cars.py
@@ -1,3 +1,4 @@
+import turtle
 from turtle import Turtle
 import random
 from scenery import COLORS
@@ -11,18 +12,26 @@ STARTING_POSITIONS = (-320, -190)
 
 
 class Cars(Turtle):
-    def __init__(self):
+    def __init__(self, numb_of_car):
         super().__init__()
-        self.cameleo = random.choice(COLORS)
-        self.compartment = []
-        self.car_mode = random.randint(1, 4)
-        self.penup()
-        self.shape("square")
-        self.color(self.cameleo)
-        self.goto(-300, -190)
+        self.cars = []
+        self.available_velocity = random.sample(range(1, 15), numb_of_car)
+        for _ in range(numb_of_car):
+            self.creation()
 
+    def creation(self):
+        car = Turtle("square")
+        col = random.choice(COLORS)
+        car.penup()
+        car.color(col)
+        car.goto(random.choice(STARTING_COR))
+        velocity = self.available_velocity.pop()
+        self.cars.append((car, velocity))
 
-
-    #def run(self):
-
-
+    def run(self):
+        for car, velocity in self.cars:
+            car.forward(velocity)
+            if car.xcor() > 320:
+                car.goto(-320, car.ycor())  # Réinitialiser la position à gauche
+                new_velocity = random.choice(range(2, 10))  # Nouvelle vitesse aléatoire
+                self.cars[self.cars.index((car, velocity))] = (car, new_velocity)  # Mettre à jour la vitesse
--- crossy_road/main.py
+++ crossy_road/main.py
@@ -1,8 +1,9 @@
+import random
 from turtle import Screen
 from crosser import Crosser
 from scoreboard import ScoreBoard
 from scenery import Scenery, COLORS
-from cars import Cars
+from cars import Cars,STARTING_COR
 #scree setup
 screen = Screen()
 screen.colormode(255)
@@ -16,10 +17,8 @@ scene = Scenery()
 screen.tracer(0)
 scene.road_maker(COLORS, 10, 32, 14, 10)
 pitt = Crosser()
-car = Cars()
+enemies = Cars(numb_of_car=len(STARTING_COR))
 board = ScoreBoard()
-
-
 #scree listen to events
 screen.listen()
 
@@ -28,7 +27,9 @@ game_is_on = True
 
 while game_is_on:
     pitt.move()
+    enemies.run()
     screen.update()
+
 
 
 #TODO : car exists and can move
